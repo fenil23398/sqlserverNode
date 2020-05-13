c54f809f4324d14258e9e4c208236bf6d1c2bc97
fenil_23 <fenilshah.osl@gmail.com>
2020-05-13 18:43:33 +0530

    first commit

---
+++ connection.js
@@ -0,0 +1,27 @@
+var db = require('mssql');
+
+//Most chances of error in connection
+//Check port it make be dynamic from sql server configuration manager
+//Change it to tcp ip and make it fix port to run and specify in connecion file
+//If not specified in config file Than It will be on port 1433
+
+var config = {
+    user : 'sa',
+    password : 'Password',
+    server : 'localhost',
+    database : 'learning',
+    options: {
+        "encrypt": true,
+        "enableArithAbort": true
+      },
+    port : 49170
+}
+
+var connection =  db.connect(config,function(err){
+    if(err)
+        console.log("Error In Connection To Database",err)
+    else    
+        console.log("Successfully Connected");
+})
+
+module.exports = connection;
