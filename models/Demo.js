7fe8c8cd1fd6455b5bb88da25b9639176e944381
fenil_23 <fenilshah.osl@gmail.com>
2020-05-16 18:35:37 +0530

    Changes for Delete Query And Joi Query Generic Select * query

--- models/Demo.js
+++ models/Demo.js
@@ -2,21 +2,40 @@ var sql = require('mssql');
 
 var request = new sql.Request(); 
 var Demo = {
-    getAllExpenses : function(){
-        return  request.query('select * from trackExpenses')
+    getAllExpenses : function(table){
+        return  request.query('select * from '+table)
     },
     addExpenses : function(expenseObj){
         var userIdd = expenseObj.userId;
         var expensee = expenseObj.expense;
+        var table = 'trackExpenses';
         // var obj = {
         //     "userId" : userId,
         //     "expense" : expense
         // };
         //console.log("Object Passed is ",obj)
-        return request.query('insert into [trackExpenses] ([userId],[expense]) values(' +userIdd+','+expensee+')')
+        return request.query('insert into '+ table + '([userId],[expense]) values(' +userIdd+','+expensee+')')
         // let sql = `insert into trackExpenses(userId,expense) VALUES()`;
         // return request.query(sql,obj);
+    },
+    deleteExpenses : function(userId){
+        return request.query('delete from [trackExpenses] where userId = '+userId);
+    },
+
+    //Type Of IN Join Query 
+
+
+    // getExpensedById : function(userId){
+    //     var tmpquery = 'select userId from userTable'
+    //     return request.query('select * from trackExpenses where userId = ' +userId+ 'and userId in (' +tmpquery + ')');
+    // }
+
+    //Cross Join
+    getExpensedById : function(userId){
+        var tmpquery = 'select userId from userTable'
+        return request.query('select trackExpenses.expense,userTable.username from trackExpenses Inner Join userTable on userTable.userId = trackExpenses.userId where trackExpenses.userId ='+userId);
     }
+
 }
 
 module.exports = Demo;
--- routes/demoRouter.js
+++ routes/demoRouter.js
@@ -2,25 +2,49 @@ var express = require('express');
 var router = express.Router();
 var Demo = require("../models/Demo");
 
-router.get('/',function(req,res,next){
-    Demo.getAllExpenses().then(rows =>{
-        console.log("Success router",rows);
-        res.json(rows)
-    })
-    .catch(err =>{
-        console.log("Error In Fetching Expenses Router")
-    })
+router.get('/:id?', function (req, res, next) {
+    if (req.params.id) {
+        Demo.getExpensedById(req.params.id)
+        .then(rows => {
+            console.log("SuccessFully Fetched By ID ",rows);
+            res.json(rows);
+        })
+        .catch(err => {
+            console.log("Error While fetching Data ",err);
+        })
+    }
+    else {
+        var tableName = 'trackExpenses';
+        Demo.getAllExpenses(tableName).then(rows => {
+            console.log("Success router", rows);
+            res.json(rows)
+        })
+            .catch(err => {
+                console.log("Error In Fetching Expenses Router")
+            })
+    }
 })
 
-router.post('/',function(req,res){
+router.post('/', function (req, res) {
     Demo.addExpenses(req.body)
-    .then(rows => {
-        console.log("Successfully added",rows);
-        res.json(rows);
-    })
-    .catch(err=>{
-        console.log("Errror While Adding Data",err)
-    })
+        .then(rows => {
+            console.log("Successfully added", rows);
+            res.json(rows);
+        })
+        .catch(err => {
+            console.log("Errror While Adding Data", err)
+        })
+})
+
+router.delete('/:id', function (req, res) {
+    Demo.deleteExpenses(req.params.id)
+        .then(rows => {
+            console.log("Deleted Successfully ", rows);
+            res.json(rows);
+        })
+        .catch(err => {
+            console.log("Error In Deleting Data ", err)
+        })
 })
 
 module.exports = router;
